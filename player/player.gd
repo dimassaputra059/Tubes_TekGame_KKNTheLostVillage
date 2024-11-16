c8000f60ac7f826d5f5c16dca3aec6d998b2fd7d
dimassaputra059 <dimas.121140059@student.itera.ac.id>
2024-11-16 17:48:25 +0700

    create player
    membuat player dan animasinya

---
+++ player/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var speed = 35
+@onready var animations = $AnimationPlayer	
+
+func handleInput():
+	var moreDirection = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity = moreDirection*speed
+	
+func updateAnimation():
+	var direction = "Idle"
+	if velocity.x < 0: direction = "Left"
+	elif velocity.x > 0: direction = "Right"
+	elif velocity.y < 0: direction = "Up"
+	elif velocity.y > 0: direction = "Down"
+	
+	animations.play("walk" + direction)
+	
+func _physics_process(delta):
+	handleInput()
+	move_and_slide()
+	updateAnimation()
