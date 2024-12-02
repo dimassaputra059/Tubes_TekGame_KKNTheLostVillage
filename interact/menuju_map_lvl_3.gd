5f7bda55c14dfb07c585caf7b6b47c7b91b26d0a
121140084_Rizki Esa Fadillah <126938786+121140084-Rizki-Esa-Fadillah@users.noreply.github.com>
2024-12-02 17:34:29 +0700

    Add files via upload

--- interact/menuju_map_lvl_3.gd
+++ interact/menuju_map_lvl_3.gd
@@ -23,7 +23,7 @@ func DialogicSignal(arg: String):
 		$".".visible = true
 		
 func change_scene():
-	get_tree().change_scene_to_file("res://map_lv_3.tscn")
+	get_tree().change_scene_to_file("res://typing/Dialogue2.tscn")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
